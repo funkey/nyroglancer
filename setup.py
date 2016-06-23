e838d51fbb44e953551e91ea7ed8dae3a70d0e46
Jan Funke <funke@ini.ch>
2016-06-23 12:48:56 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nyroglancer',
-    version='0.0.2',
+    version='0.0.3',
     description='jupyter extension for neuroglancer',
     author='Jan Funke',
     author_email='jfunke@iri.upc.edu',
