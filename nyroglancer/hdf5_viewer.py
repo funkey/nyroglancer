Fix units of offset parameter for self.add to be in nanometers, not voxels

nyroglancer/hdf5_viewer.py
@@ -35,12 +35,19 @@ class Hdf5Viewer(Viewer):
 
         kwargs = {}
         if 'offset' in dataset.attrs:
-            kwargs['offset'] = tuple(dataset.attrs['offset'][::-1])
+            offset_voxels = tuple(dataset.attrs['offset'][::-1])
+        else:
+            offset_voxels = (0, 0, 0)
         if 'resolution' in dataset.attrs:
-            kwargs['voxel_size'] = tuple(dataset.attrs['resolution'][::-1])
+            resolution = tuple(dataset.attrs['resolution'][::-1])
         elif 'voxel_size' in dataset.attrs:
-            kwargs['voxel_size'] = tuple(dataset.attrs['voxel_size'][::-1])
-        if len(dataset.shape) == 4 and dataset.shape[0] == 3:
+            resolution = tuple(dataset.attrs['voxel_size'][::-1])
+        else:
+            resolution = (1, 1, 1)
+        kwargs['voxel_size'] = resolution
+        offset_nanometers = tuple(ov * r for ov, r in zip(offset_voxels, resolution))
+        kwargs['offset'] = offset_nanometers
+        if dataset.ndim == 4 and dataset.shape[0] == 3:
             kwargs['shader'] = rgb()
 
         if dataset.dtype == np.bool: