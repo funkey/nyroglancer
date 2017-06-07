8bdd2801f442398b0df6f4e632958f18aa6d508d
Jan Funke <jfunke@iri.upc.edu>
2017-06-07 11:00:35 -0400

    Fix static package path in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license = 'Apache License 2.0',
     packages = find_packages(),
     package_data={
-        'neuroglancer.static': static_files,
+        'nyroglancer.static': static_files,
     },
     install_requires = [
         "jupyter",
